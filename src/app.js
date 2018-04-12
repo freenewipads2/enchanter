5f9b2b0833c38b36954031387fc3aaef62126f06
Nikki Sollid <nikki.sollid@learnify.se>
2018-04-12 10:45:31 +0200

    setup fixes

--- src/app.js
+++ src/app.js
@@ -1,5 +1,33 @@
+import { inject } from 'aurelia-framework';
+import { Router } from 'aurelia-router';
+import { AuthService, FetchConfig, AuthorizeStep } from 'aurelia-auth';
+import {HttpClient, json} from 'aurelia-fetch-client';
+
+@inject(AuthService,HttpClient,FetchConfig)
 export class App {
-  constructor() {
-    this.message = 'Hello World!';
+  constructor(authService,httpClient, fetchConfig) {
+    this.auth = authService;
+    this.httpClient=httpClient;
+    this.fetchConfig=fetchConfig;
   }
+
+
+  configureRouter(config, router) {
+    config.title = 'Beyond';
+    config.map([
+      {route: [''], name: 'home', moduleId: 'views/home', nav: true, title: 'Home'},
+      {
+        route: 'room/:code',
+        name: 'room',
+        moduleId: './views/room',
+        nav: false,
+        title: ""
+    },
+    ]);
+    this.router = router;
+    //this.router.refreshNavigation()
+  }
+
+
+
 }
